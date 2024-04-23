ba7a8fa09b76d41a9eb04e877140430366ffb65d
Apurba Panja <apurbapanja1@gmail.com>
2024-04-23 14:21:49 +0530

    Added Error page

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,14 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
 import RootLayout from "../layouts/RootLayout";
 import EventDiscoveryPage from "../pages/EventDiscovery.page";
+import ErrorPage from "../pages/Error.page";
+import EventInsightPage from "../pages/EventInsight.page";
 
 export const router = createBrowserRouter([
     {
         path: '/',
-        element: <RootLayout/>,
-        children: [{
-            path: '/all-events/:city',
-            element: <EventDiscoveryPage/>,
-        }],
+        element: <RootLayout />,
+        children: [
+            {
+                path: '/all-events/:city',
+                element: <EventDiscoveryPage />,
+            },
+            {
+                path: '/:slug/event',
+                element: <EventInsightPage />,
+            },
+            {
+                path: '*',
+                element: <ErrorPage />,
+            },
+        ],
     }
 ]);
